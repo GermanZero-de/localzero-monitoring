78d0d496de5dad00cff7c1aaca964eeb1e85adb3
Benedikt Grundmann <benedikt.grundmann@gmail.com>
2023-02-04 11:13:52 +0100

    Some renaming as per two scoops of django

--- config/settings.py
+++ config/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = "cpmonitor.urls"
+ROOT_URLCONF = "config.urls"
 
 TEMPLATES = [
     {
@@ -67,7 +67,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = "cpmonitor.wsgi.application"
+WSGI_APPLICATION = "config.wsgi.application"
 
 
 # Database
---
+++ cpmonitor/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CpmonitorConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "cpmonitor"
