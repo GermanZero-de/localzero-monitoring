25d32477ec6a38f74a6969e2e21672fe7424cd11
Benedikt Grundmann <benedikt.grundmann@gmail.com>
2023-02-04 11:35:36 +0100

    This sets up the basic idea of having multiple config files.  So far we have only base and debug. And are still missing the production.py file but baby steps.

---
+++ config/settings/local.py
@@ -0,0 +1,7 @@
+from .base import *
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-87=05t8of16i73x@naojx*bh(a6z7#b&3skmf#waw=_o^fs192"
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
