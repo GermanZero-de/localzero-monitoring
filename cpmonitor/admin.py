92d2e542b56ebe7933278604bb084b20a79ae648
Holger Rohlfs <h.rohlfs@outlook.com>
2023-02-04 23:14:09 +0100

    Model für Data + Lookup angelegt.
    Ohne migrations, da wir alle noch auf leerer Datenbank anfangen.

--- cpmonitor/admin.py
+++ cpmonitor/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
+from .models import City, Task, Sector, TaskCategory
 
-from .models import Sector
-
+admin.site.register(City)
+admin.site.register(Task)
 
 admin.site.register(Sector)
+admin.site.register(TaskCategory)
--- cpmonitor/models.py
+++ cpmonitor/models.py
@@ -1,10 +1,71 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Note PEP-8 naming convetions for class names apply. So use the singular and CamelCase
 
 
+### Lookup-entities (shared among all cities, entered by admins)
+
 class Sector(models.Model):
     name = models.CharField(max_length=100, unique=True)
+    ord = models.IntegerField(unique=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class TaskCategory(models.Model):
+    sector = models.ForeignKey(Sector, on_delete=models.PROTECT)
+    name = models.CharField(max_length=200, unique=True)
+    info = models.TextField()
+    ord = models.IntegerField(unique=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+### data-entities (entered by users)
+
+class City(models.Model):
+    name = models.CharField(max_length=255)
+    zipcode = models.CharField(max_length=5)
+    info = models.TextField(blank=True)  
+    url = models.URLField(blank=True)
+
+    def __str__(self) -> str:
+        return self.zipcode + " " + self.name
+
+
+class Task(models.Model):
+
+    class States(models.IntegerChoices):
+        SUGGESTED = 1
+        PLANNED = 2
+        WAITING = 3
+        IN_PROGRESS = 4
+        DONE = 5
+        REJECTED =6
+
+    class Severities(models.IntegerChoices):
+        CRITICAL = 5
+        HIGH = 4
+        STANDARD=3
+        LOW = 2
+        VERY_LOW = 1
+
+    city = models.ForeignKey(City, on_delete=models.PROTECT)
+    category = models.ForeignKey(TaskCategory, on_delete=models.PROTECT)
+    name = models.CharField(max_length=255)
+    info = models.TextField()
+
+    state = models.IntegerField(choices=States.choices)
+    severity = models.IntegerField(choices=Severities.choices)
+    completion = models.IntegerField(
+                    validators=[
+                        MinValueValidator(0),
+                        MaxValueValidator(100)
+                    ]
+                )
 
     def __str__(self) -> str:
         return self.name
