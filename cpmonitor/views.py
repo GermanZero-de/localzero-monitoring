e1e8a2bdb8a0b2948fe6510c35c26429978b69c6
Mathias de Riese <mdrie@fastmail.com>
2023-02-06 23:58:07 +0100

    Quick hack of external view - just to be able to see anything at all.

--- cpmonitor/views.py
+++ cpmonitor/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from .models import City, Task
 
 
 # Our main page
 def index(request):
-    return render(request, "index.html", {})
+    return render(
+        request,
+        "index.html",
+        {
+            "cities": City.objects.order_by("id"),
+            "tasks": Task.objects.order_by("order"),
+        },
+    )
