6fc379b7b33b1b9ef8ea61435f1879963787497d
Caroline Fischer <caroline.fischer@tngtech.com>
2023-02-13 16:47:10 +0100

    #16 configure the base url for playwright tests

--- e2e_tests/example_e2e_test.py
+++ e2e_tests/example_e2e_test.py
@@ -2,6 +2,6 @@ from playwright.sync_api import Page, expect
 
 
 def test_should_show_the_title_when_being_on_the_front_page(page: Page):
-    page.goto("http://127.0.0.1:8000/")
+    page.goto("/")
 
     expect(page).to_have_title("Klimaschutzmonitor")
