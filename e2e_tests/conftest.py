392a6fc231b668d9b0f1bf2f8f16b4e8adba71af
Caroline Fischer <caroline.fischer@tngtech.com>
2023-02-26 19:20:49 +0100

    #34 Database fixture needs async
    see https://github.com/microsoft/playwright-pytest/issues/29

--- e2e_tests/conftest.py
+++ e2e_tests/conftest.py
@@ -5,6 +5,8 @@ from django.core.management import call_command
 
 from config.settings.base import BASE_DIR
 
+os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = "true"
+
 
 @pytest.fixture(scope="session")
 def django_db_setup(django_db_setup, django_db_blocker):
