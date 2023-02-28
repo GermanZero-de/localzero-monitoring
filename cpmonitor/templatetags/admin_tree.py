14cb0781cad0cee0450168b4955969bfefa75929
Mathias de Riese <mdrie@fastmail.com>
2023-02-28 13:01:59 +0100

    Enable treebeard move in admin.
    Overwriting a template tag module causes this warning:
    ```
    ?: (templates.E003) 'admin_tree' is used for multiple template tag modules: 'cpmonitor.templatetags.admin_tree', 'treebeard.templatetags.admin_tree'
    ```

---
+++ cpmonitor/templatetags/admin_tree.py
@@ -0,0 +1,10 @@
+from treebeard.templatetags.admin_tree import result_tree as tb_result_tree
+from treebeard.templatetags.admin_tree import register
+
+
+@register.inclusion_tag("admin/tree_change_list_results.html", takes_context=True)
+def result_tree(context, cl, request):
+    """ """
+    result = tb_result_tree(context, cl, request)
+    result["filtered"] = False
+    return result
