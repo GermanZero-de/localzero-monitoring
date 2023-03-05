d170e3d8244a7e624c690c04b753b41dfe9e8999
Caroline Fischer <caroline.fischer@tngtech.com>
2023-03-05 16:31:14 +0100

    #41 settings for martor

--- config/settings/base.py
+++ config/settings/base.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "treebeard",
+    "martor",
     "cpmonitor.apps.CpmonitorConfig",
 ]
 
--- cpmonitor/urls.py
+++ cpmonitor/urls.py
@@ -1,10 +1,11 @@
-from django.urls import path
+from django.urls import path, include
 from django.contrib import admin
 
 from . import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("martor/", include("martor.urls")),
     path("", views.index, name="index"),
     path("cities/<city_name>", views.city, name="city"),
 ]
