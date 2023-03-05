154af0973594a00603e4b2d58757950bfc640e28
Caroline Fischer <caroline.fischer@tngtech.com>
2023-03-05 17:57:21 +0100

    #41 disable image upload (imgur setting) for now

--- config/settings/base.py
+++ config/settings/base.py
@@ -137,3 +137,14 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Martor (markdown editor)
+MARTOR_ENABLE_CONFIGS = {
+    "emoji": "true",
+    "imgur": "false",
+    "mention": "false",
+    "jquery": "true",
+    "living": "false",
+    "spellcheck": "false",
+    "hljs": "true",
+}
