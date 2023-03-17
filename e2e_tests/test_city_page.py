751a2e2ba51888687834a05ccb9ad76f892f1c8e
Mathias de Riese <mdrie@fastmail.com>
2023-03-17 18:07:30 +0100

    Tests corrected and extended.

--- e2e_tests/test_city_page.py
+++ e2e_tests/test_city_page.py
@@ -12,10 +12,19 @@ def test_should_go_to_city_view_when_clicking_city_select_dropdown_item(
     city_button = page.get_by_text("Beispielstadt")
     city_button.click()
 
-    expect(page).to_have_url("/cities/Beispielstadt")
+    expect(page).to_have_url("/beispielstadt/")
 
 
 def test_city_page_should_have_city_name_in_title(page: Page, django_db_setup):
-    page.goto("/cities/Beispielstadt")
+    page.goto("/beispielstadt")
 
     expect(page).to_have_title("LocalZero Monitoring Beispielstadt")
+
+
+def test_should_go_to_task_view_when_clicking_task_item(page: Page, django_db_setup):
+    page.goto("/beispielstadt")
+
+    task_title = page.get_by_text("Verkehr")
+    task_title.click()
+
+    expect(page).to_have_url("/beispielstadt/verkehr/")
---
+++ e2e_tests/test_task_page.py
@@ -0,0 +1,18 @@
+from playwright.sync_api import Page, expect
+
+
+def test_task_page_should_have_city_name_and_task_title_in_title(
+    page: Page, django_db_setup
+):
+    page.goto("/beispielstadt/verkehr")
+
+    expect(page).to_have_title("LocalZero Monitoring Beispielstadt Verkehr")
+
+
+def test_should_go_to_subtask_view_when_clicking_task_item(page: Page, django_db_setup):
+    page.goto("/beispielstadt/verkehr")
+
+    subtask_title = page.get_by_text("Radwege ausbauen")
+    subtask_title.click()
+
+    expect(page).to_have_url("/beispielstadt/verkehr/radwege-ausbauen/")
