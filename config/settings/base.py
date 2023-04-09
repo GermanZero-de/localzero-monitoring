10057db8bec952115f776709383669e98b032f73
Caroline Fischer <caroline.fischer@tngtech.com>
2023-04-09 16:53:41 +0200

    [#104] install cloudbeaver to inspect the database for debugging and maintenance
    move the database into its own folder so that we can share that folder as a volume and do not share all the other stuff

--- config/settings/base.py
+++ config/settings/base.py
@@ -87,7 +87,7 @@ WSGI_APPLICATION = "config.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "NAME": BASE_DIR / "db" / "db.sqlite3",
     }
 }
 
