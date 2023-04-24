0e3755cc53dff66d36c4c66ff774dca4adc89304
Mathias de Riese <mdrie@fastmail.com>
2023-04-24 17:43:30 +0200

    [#124] Tests for moving tasks with drag-and-drop.

--- e2e_tests/test_admin.py
+++ e2e_tests/test_admin.py
@@ -1,4 +1,5 @@
 import uuid
+from django.utils.text import slugify
 from playwright.sync_api import Page, expect
 import pytest
 
@@ -17,11 +18,12 @@ def test_should_succeed_when_logging_into_admin(page: Page, admin_login):
     page.close()
 
 
-def add_task(page, title, parent=None):
+def add_task(page: Page, title, parent=None):
     page.goto("/admin/cpmonitor/task/add/?_changelist_filters=city__id__exact%3D1")
     page.fill("[name=title]", title)
     if parent:
-        page.select_option("[name=_ref_node_id]", label=parent)
+        full_parent = page.get_by_text(parent).all_inner_texts()[0]
+        page.get_by_label("relativ zu").select_option(label=full_parent)
     page.click("[name=_save]")
 
 
@@ -70,6 +72,93 @@ def test_should_not_allow_add_when_same_title_in_same_sector(page: Page, admin_l
     errlist = page.locator(".errorlist")
     expect(errlist.get_by_role("listitem")).to_contain_text(
         "Das kollidiert mit einem anderen Eintrag."
+    )
+
+    page.close()
+
+
+def drag_task_from_to(page, source, target):
+    target_locator = page.locator(
+        f"//*[@class='field-title' and contains(., '{target}')]"
+    )
+
+    drag_handler = page.locator(
+        f"//th[@class='field-title' and contains(., '{source}')]/preceding-sibling::td[@class='drag-handler']/span"
+    )
+
+    drag_handler.drag_to(
+        target_locator,
+        target_position={"x": 10, "y": 50},
+        force=True,
+        timeout=1000,
+    )
+
+
+def test_should_move_and_adjust_slugs_when_dragged(page: Page, admin_login):
+    uid = str(uuid.uuid4())
+
+    sector1 = "Admin Test 1 " + uid
+    sector2 = "Admin Test 2 " + uid
+    task = "To be dragged " + uid
+    sub_task = "Has to be adjusted " + uid
+    sub_sub_task = "Has to be adjusted " + uid
+
+    slug_s1 = slugify(sector1)
+    slug_s2 = slugify(sector2)
+    sub_sub_slug = (
+        "/" + slugify(task) + "/" + slugify(sub_task) + "/" + slugify(sub_sub_task)
+    )
+
+    add_task(page, sector1)
+    add_task(page, task, sector1)
+    add_task(page, sub_task, task)
+    add_task(page, sub_sub_task, sub_task)
+
+    add_task(page, sector2)
+
+    # Make sure, all are visible and not hidden on page 2.
+    page.goto("/admin/cpmonitor/task/?all=&city__id__exact=1")
+
+    # Without exact=True this would also catch the sub-tasks:
+    expect(page.get_by_text(slug_s1 + "/" + slugify(task), exact=True)).to_be_visible()
+    # ...on the other hand, this should catch any sub-tasks:
+    expect(page.get_by_text(slug_s2 + "/" + slugify(task))).not_to_be_visible()
+    expect(page.get_by_text(slug_s1 + sub_sub_slug)).to_be_visible()
+    expect(page.get_by_text(slug_s2 + sub_sub_slug)).not_to_be_visible()
+
+    drag_task_from_to(page, task, sector2)
+
+    expect(page.locator(".messagelist")).to_contain_text("positioniert unterhalb von")
+
+    expect(page.get_by_text(slug_s1 + "/" + slugify(task))).not_to_be_visible()
+    expect(page.get_by_text(slug_s2 + "/" + slugify(task), exact=True)).to_be_visible()
+    expect(page.get_by_text(slug_s1 + sub_sub_slug)).not_to_be_visible()
+    expect(page.get_by_text(slug_s2 + sub_sub_slug)).to_be_visible()
+
+    page.close()
+
+
+def test_should_not_allow_move_when_same_title_in_same_sector(page: Page, admin_login):
+    uid = str(uuid.uuid4())
+
+    sector1 = "Admin Test 1 " + uid
+    sector2 = "Admin Test 2 " + uid
+    task1 = "Personal Einstellen " + uid
+    task2 = "personal einstellen " + uid
+
+    add_task(page, sector1)
+    add_task(page, task1, sector1)
+
+    add_task(page, sector2)
+    add_task(page, task2, sector2)
+
+    # Make sure all tasks are visible
+    page.goto("/admin/cpmonitor/task/?all=&city__id__exact=1")
+
+    drag_task_from_to(page, task2, sector1)
+
+    expect(page.locator(".messagelist")).to_contain_text(
+        "Es gibt bereits einen Sektor / eine Maßnahme mit der URL "
     )
 
     page.close()
