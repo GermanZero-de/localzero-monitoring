37cb957f5a9c88ce809db4fc454649c11e154604
Mathias de Riese <mdrie@fastmail.com>
2023-04-27 13:04:07 +0200

    Fix upload when using docker: Let `container.py` settings take precedence over `base.py` (and `local.py`...)

--- config/settings/production-container.py
+++ config/settings/production-container.py
@@ -1,6 +1,6 @@
 import platform
-from .container import *
 from .production import *
+from .container import *
 from pathlib import Path
 
 # only adjust the production settings for containers via imports
