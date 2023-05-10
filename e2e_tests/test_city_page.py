df38db52737387061932e0d74ed7747c6688769c
Caroline Fischer <coralinefischer@gmail.com>
2023-05-10 17:49:24 +0200

    [#72] test climate action plan checklist visibility

--- e2e_tests/test_city_page.py
+++ e2e_tests/test_city_page.py
@@ -28,3 +28,15 @@ def test_should_go_to_task_view_when_clicking_task_item(live_server, page: Page)
     task_title.click()
 
     expect(page).to_have_url(live_server.url + "/beispielstadt/mobilitat/")
+
+
+def test_city_page_should_show_climate_action_plan_checklist_when_values_for_the_checklist_exist(
+    live_server, page: Page
+):
+    page.goto(live_server.url + "/beispielstadt/")
+
+    expect(page.get_by_text("Klimaaktionsplan (KAP) Checkliste")).to_be_visible()
+
+    page.goto(live_server.url + "/ohnenix/")
+
+    expect(page.get_by_text("Klimaaktionsplan (KAP) Checkliste")).not_to_be_visible()
