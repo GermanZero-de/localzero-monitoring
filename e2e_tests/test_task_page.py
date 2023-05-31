1bbe49ae8132ae6ecaad61cff7dfeeeed4adb16a
Caroline Fischer <coralinefischer@gmail.com>
2023-05-31 21:53:08 +0200

    [#179] Adapt tests to the new frontend design

--- e2e_tests/test_task_page.py
+++ e2e_tests/test_task_page.py
@@ -2,17 +2,16 @@ from playwright.sync_api import Page, expect
 
 
 def test_the_task_page_should_have_the_city_name_in_the_title(live_server, page: Page):
-    page.goto(live_server.url + "/beispielstadt/mobilitat/")
+    page.goto(live_server.url + "/beispielstadt/massnahmen/mobilitat/")
 
     expect(page).to_have_title("LocalZero Monitoring - Beispielstadt")
 
 
 def test_should_go_to_subtask_view_when_clicking_task_item(live_server, page: Page):
-    page.goto(live_server.url + "/beispielstadt/mobilitat/")
+    page.goto(live_server.url + "/beispielstadt/massnahmen/mobilitat/")
 
-    subtask_title = page.get_by_text("Radwege ausbauen")
-    subtask_title.click()
+    page.get_by_text("Radwege ausbauen").click()
 
     expect(page).to_have_url(
-        live_server.url + "/beispielstadt/mobilitat/radwege-ausbauen/"
+        live_server.url + "/beispielstadt/massnahmen/mobilitat/radwege-ausbauen/"
     )
