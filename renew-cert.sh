bff90670d27eb4e74a6031de23cd02f1835c6c8d
Felix Lampe <mail@flampe.de>
2023-06-01 18:50:14 +0200

    [#30] Configure nginx to use full cert chain instead of only our cert

--- renew-cert.sh
+++ renew-cert.sh
@@ -5,5 +5,5 @@ set -x
 
 docker exec acme-sh --renew -d monitoring-test.localzero.net -d monitoring.localzero.net \
     --standalone --server https://acme-v02.api.letsencrypt.org/directory \
-    --cert-file /acme.sh/ssl-cert.cer --key-file /acme.sh/ssl-cert.key
+    --fullchain-file /acme.sh/fullchain.cer --key-file /acme.sh/ssl-cert.key
 docker exec nginx-testing nginx -s reload
