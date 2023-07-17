e6bb8fd7ea056b31e2dce286920d2d0d485df9f4
Holger-GZ <holger.rohlfs@germanzero.de>
2023-07-17 12:57:09 +0200

    Modified Header/Footer/Menu to localZero default style. closes #202

---
+++ cpmonitor/static/js/localzero.js
@@ -0,0 +1,12 @@
+function toggleMobile() {
+    document.getElementById('site-menu').classList.toggle('shownow');
+}
+
+function toggleSubmenu(event) {
+    var liElement = event.currentTarget;
+    var ulElement = liElement.querySelector('ul'); // Find the first <ul> child element
+
+    if (ulElement) {
+        ulElement.classList.toggle('show-sub-menu'); // Toggle the class 'submenu-open' on the <ul> element
+    }
+}
