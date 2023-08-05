cc93e99957d9e4b7236f8b7be57eaf05a09c7689
Felix Lampe <mail@flampe.de>
2023-08-05 16:40:59 +0200

    [#21] Fix image tags and add naive error handling during SSH

--- deploy.sh
+++ deploy.sh
@@ -30,19 +30,16 @@ docker save klimaschutzmonitor-dbeaver -o klimaschutzmonitor-dbeaver.tar
 # 5
 scp -C cpmonitor.tar klimaschutzmonitor-dbeaver.tar docker-compose.yml crontab renew-cert.sh monitoring@monitoring.localzero.net:/tmp/
 
-# 6 - TODO: fail on error also within the SSH session (move this part to an scp'd script..?)
 ssh -tt lzm /bin/bash << EOF
+set -euo pipefail
 
 # 7
 docker load -i /tmp/cpmonitor.tar
 docker load -i /tmp/klimaschutzmonitor-dbeaver.tar
 
 # 8
-docker tag cpmonitor:latest cpmonitor:${date}${tag_suffix}
-docker tag cpmonitor:latest cpmonitor:${env}
-
-docker tag klimaschutzmonitor-dbeaver:latest klimaschutzmonitor-dbeaver:${date}${tag_suffix}
-docker tag klimaschutzmonitor-dbeaver:latest klimaschutzmonitor-dbeaver:${env}
+docker tag cpmonitor:${env} cpmonitor:${date}${tag_suffix}
+docker tag klimaschutzmonitor-dbeaver:${env} klimaschutzmonitor-dbeaver:${date}${tag_suffix}
 
 # 9
 cd ~/${env}/
@@ -51,10 +48,7 @@ docker-compose down --volumes
 cp -v db/db.sqlite3 /data/LocalZero/DB_BACKUPS/${env}/db.sqlite3.${date}${tag_suffix}
 cp -vr cpmonitor/images/uploads /data/LocalZero/DB_BACKUPS/testing/uploads.${date}${tag_suffix}
 # apply migrations using a temporary container
-docker run --user=1007:1007 --rm -it -v /home/monitoring/${env}/db:/db cpmonitor:latest sh
-DJANGO_SECRET_KEY=whatever DJANGO_CSRF_TRUSTED_ORIGINS=https://whatever DJANGO_DEBUG=False python manage.py migrate --settings=config.settings.container
-# exit and stop the temporary container
-exit
+docker run --user=1007:1007 --rm -it -v /home/monitoring/${env}/db:/db cpmonitor:${env} sh -c "DJANGO_SECRET_KEY=whatever DJANGO_CSRF_TRUSTED_ORIGINS=https://whatever DJANGO_DEBUG=False python manage.py migrate --settings=config.settings.container"
 
 # TODO: it stops here for some reason..
 
