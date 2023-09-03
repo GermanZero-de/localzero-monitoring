c7837a1669bb61458fd43d5c138f8ee87a4b44c2
Caroline Fischer <coralinefischer@gmail.com>
2023-09-03 12:43:53 +0200

    test checklist item expansion

--- e2e_tests/test_cap_checklist_page.py
+++ e2e_tests/test_cap_checklist_page.py
@@ -84,3 +84,54 @@ def test_should_show_the_correct_helptext_for_cap_assessment(live_server, page: 
 
     # First one should be not visible again
     expect(checked_question_help_text_locator).to_be_hidden()
+
+
+def test_should_expand_and_unexpand_the_help_text_when_clicking_on_a_checklist_item(
+    live_server, page: Page
+):
+    page.goto(live_server.url + "/beispielstadt/kap_checkliste/")
+
+    checklist_item = page.get_by_role(
+        "button",
+        name="Bilanziert der Klima-Aktionsplan in den Sektoren der Klimavision?",
+    )
+    help_text = page.locator(
+        ".accordion-collapse",
+        has_text="Die Klimavision beinhaltet die Sektoren Strom, Wärme, Verkehr, Industrie, Gebäude, Abfall",
+    )
+
+    expect(help_text).not_to_be_visible()
+
+    checklist_item.click()
+
+    expect(help_text).to_be_visible()
+
+
+def test_should_only_expand_the_last_helptext_when_clicking_on_two_checklist_items(
+    live_server, page: Page
+):
+    page.goto(live_server.url + "/beispielstadt/kap_checkliste/")
+
+    checklist_item1 = page.get_by_role(
+        "button",
+        name="Bilanziert der Klima-Aktionsplan in den Sektoren der Klimavision?",
+    )
+    help_text1 = page.locator(
+        ".accordion-collapse",
+        has_text="Die Klimavision beinhaltet die Sektoren Strom, Wärme, Verkehr, Industrie, Gebäude, Abfall",
+    )
+
+    checklist_item2 = page.get_by_role(
+        "button",
+        name="Enthält der Klima-Aktionsplan ein Szenario mit dem Ziel Klimaneutralität bis 2035?",
+    )
+    help_text2 = page.locator(
+        ".accordion-collapse",
+        has_text="Das Szenario soll zeigen wie die Kommune unter realistischen Bedinungen",
+    )
+
+    checklist_item1.click()
+    checklist_item2.click()
+
+    expect(help_text1).not_to_be_visible()
+    expect(help_text2).to_be_visible()
