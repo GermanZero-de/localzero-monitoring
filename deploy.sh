75c65f4761b6cbaaa383d33345882d54ea7bccd0
Felix Lampe <mail@flampe.de>
2023-09-12 22:01:35 +0200

    [#21] Fix production deployment having no effect

--- deploy.sh
+++ deploy.sh
@@ -26,7 +26,7 @@ echo "Tagging version as $tag in git."
 git tag -a $tag -m "Deployment to ${env}" && git push origin $tag
 
 #3
-docker compose build
+docker compose --env-file .env.${env} build
 
 # 4
 docker save cpmonitor -o cpmonitor.tar
@@ -43,7 +43,6 @@ docker load -i /tmp/cpmonitor.tar
 docker load -i /tmp/klimaschutzmonitor-dbeaver.tar
 
 # 8
-# TODO something about the tagging doesn't work as it should, do local tags not match what we expect here?
 docker tag cpmonitor:${env} cpmonitor:${date}${tag_suffix}
 docker tag klimaschutzmonitor-dbeaver:${env} klimaschutzmonitor-dbeaver:${date}${tag_suffix}
 
@@ -54,7 +53,6 @@ docker-compose down --volumes
 cp -v db/db.sqlite3 /data/LocalZero/DB_BACKUPS/${env}/db.sqlite3.${date}${tag_suffix}
 cp -vr cpmonitor/images/uploads /data/LocalZero/DB_BACKUPS/${env}/uploads.${date}${tag_suffix}
 # apply migrations using a temporary container
-# this doesn't seem to work like it should; migrations were not applied last time
 docker run --user=1007:1007 --rm -v /home/monitoring/${env}/db:/db cpmonitor:${env} sh -c "DJANGO_SECRET_KEY=whatever DJANGO_CSRF_TRUSTED_ORIGINS=https://whatever DJANGO_DEBUG=False python manage.py migrate --settings=config.settings.container"
 
 # use the latest docker-compose.yml to start the app using the new image
