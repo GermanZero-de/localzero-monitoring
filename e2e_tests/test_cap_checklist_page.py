045a4603dcaac05c4a181f7c3a0bfa4ac090f31c
Patrick Schmitz <patrick.schmitz@mail.de>
2023-09-22 13:59:07 +0200

    [#253] Add tests for rationale

--- e2e_tests/test_cap_checklist_page.py
+++ e2e_tests/test_cap_checklist_page.py
@@ -34,25 +34,31 @@ def test_should_show_the_cap_assessment_and_the_checklist(live_server, page: Pag
     ).to_have_class("lz-icon EMPTY")
 
 
-def test_should_expand_and_unexpand_the_help_text_when_clicking_on_a_checklist_item(
+def test_should_expand_the_help_text_and_rationale_when_clicking_on_a_checklist_item(
     live_server, page: Page
 ):
     page.goto(live_server.url + "/beispielstadt/kap_checkliste/")
 
     checklist_item = page.get_by_role(
         "button",
-        name="Bilanziert der Klima-Aktionsplan in den Sektoren der Klimavision?",
+        name="Ist ein Trendszenario hinterlegt?",
     )
     help_text = page.locator(
         ".accordion-collapse",
-        has_text="Die Klimavision beinhaltet die Sektoren Strom, Wärme, Verkehr, Industrie, Gebäude, Abfall",
+        has_text="Ein Trendszenario ist ein Szenario, welches die Treibhausgas-Emissionen",
+    )
+    rationale = page.locator(
+        ".accordion-collapse",
+        has_text="Trendszenario ist in Arbeit",
     )
 
     expect(help_text).not_to_be_visible()
+    expect(rationale).not_to_be_visible()
 
     checklist_item.click()
 
     expect(help_text).to_be_visible()
+    expect(rationale).to_be_visible()
 
 
 def test_should_only_expand_the_latest_helptext_when_clicking_on_two_different_checklist_items(
