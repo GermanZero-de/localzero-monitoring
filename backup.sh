2da3b85bef90b606290433f47dc33150cdc39a69
Caroline Fischer <coralinefischer@gmail.com>
2023-10-28 13:08:17 +0200

    [#135] ugly file permission stuff

--- backup.sh
+++ backup.sh
@@ -14,21 +14,21 @@ source .env.${env}
 # define place and number of backups
 directoryOfBackupInContainer="/backup"
 directoryOfBackup=$BACKUP_PATH
-maxNumberOfBackups=7
+maxNumberOfBackups=3
 
 date=$(date +%Y-%m-%d-%H-%M-%S)
 
 # create a new backup
-docker exec djangoapp-testing cd /backup
-docker exec djangoapp-testing mkdir /$date
-docker exec djangoapp-testing sqlite3 /db/db.sqlite3 ".backup 'backup_db.sqlite3'"
-# docker exec djangoapp-testing /bin/sh -c "python -Xutf8 manage.py dumpdata -e contenttypes -e auth.Permission -e admin.LogEntry -e sessions --indent 2 --settings=config.settings.container > $directoryOfBackupInContainer/$date/database.json"
+mkdir -m a+rw "$directoryOfBackup"/"$date"
+docker exec djangoapp-testing sqlite3 /db/db.sqlite3 ".backup '$directoryOfBackupInContainer/$date/backup_db.sqlite3'"
 docker exec djangoapp-testing /bin/sh -c "python manage.py mediabackup --settings=config.settings.container -O $directoryOfBackupInContainer/$date/media.tar"
+docker exec djangoapp-testing chmod a+rw "$directoryOfBackupInContainer"/"$date"/backup_db.sqlite3
+docker exec djangoapp-testing chmod a+rw "$directoryOfBackupInContainer"/"$date"/media.tar
 
-printf "Created a new backup in %s \n" $directoryOfBackup/$date
+printf "Created a new backup in %s \n" "$directoryOfBackup"/"$date"
 
 # delete the oldest backups
-cd $directoryOfBackup || { echo "Could not enter directory $directoryOfBackup"; exit 126; }
+cd "$directoryOfBackup"
 
 existingBackupFolders=$( ls -d *-*-*-*-*-* )
 numberOfBackups=$( echo "$existingBackupFolders" | wc -l )
@@ -36,7 +36,7 @@ numberOfBackups=$( echo "$existingBackupFolders" | wc -l )
 while [ $numberOfBackups -gt $maxNumberOfBackups ]
 do
   oldestBackupFolder=$( echo "$existingBackupFolders" | head -1 )
-  rm -r $oldestBackupFolder
+  rm -r "$oldestBackupFolder"
   printf "Deleted old backup: %s \n" "$directoryOfBackup/$oldestBackupFolder"
 
   existingBackupFolders=$( ls -d *-*-*-*-*-* )
