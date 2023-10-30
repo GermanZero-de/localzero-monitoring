12858e3b7b4b18c0878c16bf0bb357e360390426
Caroline Fischer <coralinefischer@gmail.com>
2023-10-30 18:30:01 +0100

    [#227] Script for running the testing app with prod data

--- deploy.sh
+++ deploy.sh
@@ -35,7 +35,7 @@ docker save cpmonitor:${env} -o cpmonitor.tar
 docker save klimaschutzmonitor-dbeaver:${env} -o klimaschutzmonitor-dbeaver.tar
 
 # Copy the images, the compose files, the certificate renewal cron job and the reverse proxy settings to the server
-scp -C -r cpmonitor.tar klimaschutzmonitor-dbeaver.tar docker-compose.yml crontab reload-cert.sh backup.sh docker/reverseproxy/ monitoring.localzero.net:/tmp/
+scp -C -r cpmonitor.tar klimaschutzmonitor-dbeaver.tar docker-compose.yml crontab reload-cert.sh backup.sh start-testing-with-prod-data.sh docker/reverseproxy/ monitoring.localzero.net:/tmp/
 
 # Login to the server and execute everything that follows there
 ssh -tt monitoring.localzero.net /bin/bash << EOF
@@ -75,9 +75,9 @@ crontab /tmp/crontab
 cp /tmp/reload-cert.sh /home/monitoring/
 chmod +x /home/monitoring/reload-cert.sh
 
-# make backup script available
-cp /tmp/backup.sh /home/monitoring/
-chmod +x /home/monitoring/backup.sh
+# make scripts available
+cp /tmp/backup.sh /tmp/start-testing-with-prod-data.sh /home/monitoring/
+chmod +x /home/monitoring/backup.sh /home/monitoring/start-testing-with-prod-data.sh
 
 echo 'FINISHED SUCCESSFULLY!'
 
