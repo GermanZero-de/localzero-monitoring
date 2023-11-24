328b5c8366304e8db20dfe0902fd502bf543c072
Caroline Fischer <coralinefischer@gmail.com>
2023-11-24 16:05:29 +0100

    [#310] update django-allauth

--- config/settings/base.py
+++ config/settings/base.py
@@ -63,6 +63,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "allauth.account.middleware.AccountMiddleware",
 ]
 
 AUTHENTICATION_BACKENDS = (
