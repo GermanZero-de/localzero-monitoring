a508f1008956ebc7afbc297428a35f799bb56c23
51moon <gh51moon@outlook.com>
2024-01-03 21:54:08 +0100

    correct previous mistake

--- e2e_tests/test_deployed.py
+++ e2e_tests/test_deployed.py
@@ -8,7 +8,7 @@ def test_should_provide_basic_website_functionality_when_using_the_deployed_appl
     This test requires starting the application with the local configuration and creating the database with migrations
     applied and `e2e_tests/database/test_database.json` loaded.
     """
-    page.goto("https://localhost/deutschland")
+    page.goto("https://localhost")
 
     page.locator(".card", has_text="Beispielstadt").click()
 
