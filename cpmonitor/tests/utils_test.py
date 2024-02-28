e1e919483dc82537489cb874ae24e32abd892d3b
Felix Lampe <mail@flampe.de>
2024-02-28 21:37:34 +0100

    [#399] Add some more time calc tests

--- cpmonitor/tests/utils_test.py
+++ cpmonitor/tests/utils_test.py
@@ -3,8 +3,22 @@ from freezegun import freeze_time
 from cpmonitor.utils import RemainingTimeInfo
 
 
-@freeze_time("2025-01-01")
+@freeze_time("2025-01-02")
 def test_given_days_in_year_left_exceed_half_a_year_then_remaining_time_is_calculated_correctly():
-    remaining = RemainingTimeInfo(date(2025, 1, 1), 2025)
-    assert remaining.years_left == 0
-    assert remaining.days_in_year_left == 365
+    one_full_year_remaining = RemainingTimeInfo(date(2025, 1, 2), 2025)
+    assert one_full_year_remaining.years_left == 0
+    assert one_full_year_remaining.days_in_year_left == 364
+
+
+@freeze_time("2025-01-01")
+def test_given_days_in_year_left_is_exactly_a_year_then_remaining_time_is_calculated_correctly():
+    one_full_year_remaining = RemainingTimeInfo(date(2025, 1, 1), 2025)
+    assert one_full_year_remaining.years_left == 1
+    assert one_full_year_remaining.days_in_year_left == 0
+
+
+@freeze_time("2025-12-31")
+def test_given_days_in_year_left_is_single_day_then_remaining_time_is_calculated_correctly():
+    one_full_year_remaining = RemainingTimeInfo(date(2025, 12, 31), 2025)
+    assert one_full_year_remaining.years_left == 0
+    assert one_full_year_remaining.days_in_year_left == 1
