7e0003655c8011e299daaadb1cdd9bdec6c6ed5b
Caroline Fischer <coralinefischer@gmail.com>
2024-03-09 19:16:34 +0100

    [#405] test new cap edit page

--- e2e_tests/test_admin.py
+++ e2e_tests/test_admin.py
@@ -10,6 +10,11 @@ def admin_login(base_url: str, page: Page):
     page.get_by_text("Anmelden").click()
 
 
+def go_to_cap_edit_board(page: Page, city: str):
+    page.get_by_text("KAP editieren").click()
+    page.get_by_text(city).click()
+
+
 def add_task(base_url: str, page: Page, title, parent=None):
     page.goto(
         base_url + "/admin/cpmonitor/task/add/?_changelist_filters=city__id__exact%3D1"
@@ -21,8 +26,41 @@ def add_task(base_url: str, page: Page, title, parent=None):
     page.get_by_role("button", name="Sichern", exact=True).first.click()
 
 
-def drag_task(page, task_to_drag: str, target_task: str):
+def drag_task_on_task_cap_board(
+    page: Page, task_to_drag_title: str, target_task_title: str
+):
+    task_to_drag = page.get_by_text(task_to_drag_title)
+    target_task = page.locator(".cap_board__task-item", has_text=target_task_title)
+    target_task.scroll_into_view_if_needed()
+
+    to_drag_x, to_drag_y = get_position_in_the_middle(task_to_drag)
+    target_x, target_y = get_position_in_the_lower_third(target_task)
+
+    page.mouse.move(to_drag_x, to_drag_y)
+    page.mouse.down()
+    page.mouse.move(target_x, target_y)
+    page.mouse.up()
+
+
+def drag_task_above_task_cap_board(
+    page: Page, task_to_drag_title: str, target_task_title: str
+):
+    task_to_drag = page.get_by_text(task_to_drag_title)
+    target_task = page.locator(".cap_board__task-item", has_text=target_task_title)
+    target_task.scroll_into_view_if_needed()
+
+    to_drag_x, to_drag_y = get_position_in_the_middle(task_to_drag)
+    target_x, target_y = get_position_in_the_upper_third(target_task)
+
+    page.mouse.move(to_drag_x, to_drag_y)
+    page.mouse.down()
+    page.mouse.move(target_x, target_y)
+    page.mouse.up()
+
+
+def drag_task(page: Page, task_to_drag: str, target_task: str):
     target_locator = page.get_by_role("link", name=target_task)
+
     drag_handler = page.locator(
         f"//th[@class='field-title' and contains(., '{task_to_drag}')]/preceding-sibling::td[@class='drag-handler']/span"
     )
@@ -48,6 +86,13 @@ def get_position_in_the_lower_third(target_locator):
     target_bounding_box = target_locator.bounding_box()
     target_x = target_bounding_box["x"] + target_bounding_box["width"] / 2
     target_y = target_bounding_box["y"] + target_bounding_box["height"] * 2 / 3
+    return target_x, target_y
+
+
+def get_position_in_the_upper_third(target_locator):
+    target_bounding_box = target_locator.bounding_box()
+    target_x = target_bounding_box["x"] + target_bounding_box["width"] / 2
+    target_y = target_bounding_box["y"] + target_bounding_box["height"] * 1 / 3
     return target_x, target_y
 
 
@@ -173,3 +218,88 @@ def test_should_allow_local_groups_without_image(live_server, page: Page):
     page.locator("#id_local_group-0-name").fill("OhnenixZero")
     page.get_by_role("button", name="Sichern", exact=True).first.click()
     expect(page.get_by_text("erfolgreich geändert")).to_be_visible()
+
+
+def test_should_add_the_task_to_the_chosen_sector_when_clicking_add_new_task(
+    live_server, page: Page
+):
+    # given
+    sector_id = "#column-7"
+    new_task_title = "Maßnahme 1"
+
+    # when
+    admin_login(live_server.url, page)
+    go_to_cap_edit_board(page, "Beispielstadt")
+    page.locator(sector_id).get_by_role("link", name="Neue Maßnahme hinzufügen").click()
+
+    page.fill("[name=title]", new_task_title)
+    page.get_by_role("button", name="Sichern", exact=True).first.click()
+
+    # then
+    go_to_cap_edit_board(page, "Beispielstadt")
+    column = page.locator(sector_id)
+    new_task = column.get_by_text(new_task_title)
+    expect(new_task).to_be_visible()
+
+
+def test_should_delete_the_task_when_clicking_on_the_delete_button(
+    live_server, page: Page
+):
+    # given
+    task_title = "Klimaaktionsplan"
+
+    # when
+    admin_login(live_server.url, page)
+    go_to_cap_edit_board(page, "Beispielstadt")
+    page.locator(".cap_board__task-item", has_text=task_title).get_by_text(
+        "Löschen"
+    ).click()
+    page.get_by_text("Ja, ich bin sicher").click()
+
+    # then
+    go_to_cap_edit_board(page, "Beispielstadt")
+    expect(page.get_by_text(task_title)).not_to_be_visible()
+
+
+def test_should_move_the_task_to_the_top_level_of_the_chosen_sector_when_moving_a_task_above_another_task(
+    live_server, page: Page
+):
+    # given
+    sector_id = "#column-7"
+    task_title = "Jobtickets für Verwaltung"
+    target_task_title = "Klimaaktionsplan"
+
+    # when
+    admin_login(live_server.url, page)
+    go_to_cap_edit_board(page, "Beispielstadt")
+    drag_task_above_task_cap_board(page, task_title, target_task_title)
+
+    # then
+    column = page.locator(sector_id)
+    dragged_task = column.locator(".cap_board__task-item", has_text=task_title)
+    expect(dragged_task).to_be_visible()
+    expect(dragged_task.locator("//parent::div").get_by_role("img")).to_have_class(
+        "inline marginleft-0"
+    )
+
+
+def test_should_move_the_task_below_the_chosen_task_when_moving_a_task_on_another_task(
+    live_server, page: Page
+):
+    # given
+    sector_id = "#column-7"
+    task_title = "Jobtickets für Verwaltung"
+    target_task_title = "Klimaaktionsplan"
+
+    # when
+    admin_login(live_server.url, page)
+    go_to_cap_edit_board(page, "Beispielstadt")
+    drag_task_on_task_cap_board(page, task_title, target_task_title)
+
+    # then
+    column = page.locator(sector_id)
+    dragged_task = column.locator(".cap_board__task-item", has_text=task_title)
+    expect(dragged_task).to_be_visible()
+    expect(dragged_task.locator("//parent::div").get_by_role("img")).to_have_class(
+        "inline marginleft-1"
+    )
