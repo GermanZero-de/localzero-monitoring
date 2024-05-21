01e8b97ff91839bdabd506a8216e53409ec29a49
Caroline Fischer <coralinefischer@gmail.com>
2024-05-21 17:23:41 +0200

    [#443] style the tiles

--- frontend/app/components/Tile.tsx
+++ frontend/app/components/Tile.tsx
@@ -2,7 +2,7 @@
 import * as React from "react";
 import styles from "./styles/Tile.module.scss";
 import Image from "next/image";
-import logo from "../../public/images/stadt.png";
+import arrow from "../../public/images/arrow-right.svg";
 
 const icons = {
   facebook:
@@ -11,17 +11,37 @@ const icons = {
 
 type Props = {
   name: string;
+  logo: string;
 };
 
-const Tile: React.FC<Props> = ({ name }) => {
+const Tile: React.FC<Props> = ({ name, logo }) => {
+  let image = null;
+
+  if (!!logo) {
+    image = (
+      <div className={styles.image}>
+        <Image
+          width={100}
+          height={100}
+          src={"http://127.0.0.1:8000" + logo}
+          alt={"Logo von " + name}
+        />
+      </div>
+    );
+  }
+
   return (
     <div className={styles.wrapper}>
-      {name}
-      <Image
-        width={100}
-        src={logo}
-        alt={name}
-      />
+      <div className={styles.heading}>
+        {name}
+        <Image
+          width={30}
+          height={30}
+          src={arrow}
+          alt="arrow right"
+        ></Image>
+      </div>
+      {image}
     </div>
   );
 };
--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+          {
+            protocol: "http",
+            hostname: "127.0.0.1",
+            port: "8000",
+            pathname: "/**",
+          },
+        ],
+      },
+};
 
 export default nextConfig;
