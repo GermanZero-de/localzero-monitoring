c4f929957471428890d6455f7a47f1ea049054f9
Caroline Fischer <coralinefischer@gmail.com>
2024-05-31 08:10:00 +0200

    [#457] implement the cap checklist page
    * add a serializer for the cap checklist

--- cpmonitor/serializers.py
+++ cpmonitor/serializers.py
@@ -1,4 +1,4 @@
-from cpmonitor.models import City, LocalGroup
+from cpmonitor.models import City, LocalGroup, CapChecklist
 from rest_framework import serializers
 
 #
@@ -20,8 +20,36 @@ class LocalGroupSerializer(serializers.ModelSerializer):
         ]
 
 
+class CapChecklistSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CapChecklist
+        fields = "__all__"
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+
+        checkbox_items = [
+            field
+            for field in instance._meta.fields
+            if field.attname not in ["city_id", "id"]
+            and "_rationale" not in field.attname
+        ]
+
+        return [
+            {
+                "id": idx,
+                "question": checkbox_item.verbose_name,
+                "is_checked": getattr(instance, checkbox_item.attname),
+                "help_text": checkbox_item.help_text,
+                "rationale": getattr(instance, checkbox_item.attname + "_rationale"),
+            }
+            for idx, checkbox_item in enumerate(checkbox_items)
+        ]
+
+
 class CitySerializer(serializers.ModelSerializer):
     local_group = LocalGroupSerializer(read_only=True)
+    cap_checklist = CapChecklistSerializer(read_only=True)
 
     class Meta:
         model = City
@@ -40,4 +68,6 @@ class CitySerializer(serializers.ModelSerializer):
             "supporting_ngos",
             "slug",
             "local_group",
+            "cap_checklist",
+            "assessment_action_plan",
         ]
---
+++ frontend/app/components/ChecklistItem.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import "bootstrap-icons/font/bootstrap-icons.css";
+import { AccordionBody, AccordionHeader, AccordionItem } from "react-bootstrap";
+import Markdown from "react-markdown";
+import styles from "./styles/CapChecklistItem.module.scss";
+
+type Props = {
+  checklist_item: {
+    id: string;
+    is_checked: boolean;
+    question: string;
+    help_text: string;
+    rationale: string;
+  };
+};
+
+const ChecklistItem: React.FC<Props> = ({ checklist_item }) => {
+  return (
+    <AccordionItem eventKey={checklist_item.id}>
+      <AccordionHeader className={styles.accordionHeader}>
+        {checklist_item.is_checked ? (
+          <i className={`bi-check-circle pe-3 ${styles.checked}`}></i>
+        ) : (
+          <i className={`bi-circle pe-3 ${styles.unchecked}`}></i>
+        )}
+        {checklist_item.question}
+      </AccordionHeader>
+      <AccordionBody>
+        {checklist_item.help_text.trim() ? (
+          <div>
+            <strong>Erklärung:</strong>
+            <Markdown>{checklist_item.help_text}</Markdown>
+          </div>
+        ) : (
+          <></>
+        )}
+        {checklist_item.rationale.trim() ? (
+          <div>
+            <strong>Anmerkung / Begründung: </strong>
+            <Markdown>{checklist_item.rationale}</Markdown>
+          </div>
+        ) : (
+          <></>
+        )}
+      </AccordionBody>
+    </AccordionItem>
+  );
+};
+
+export default ChecklistItem;
