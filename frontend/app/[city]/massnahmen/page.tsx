7e76c081781c581cba15c7684bef305edddeef68
Elena Jeron <elena.jeron@gmail.com>
2024-07-24 23:21:25 +0200

    454 Massnahmen karte

--- frontend/app/[city]/massnahmen/page.tsx
+++ frontend/app/[city]/massnahmen/page.tsx
@@ -1,9 +1,17 @@
 "use client";
 
 import { useGetCity } from "@/app/CityService";
+import Image from "next/image";
+
 import MeasureCard from "@/app/components/MeasureCard";
 import { usePathname } from "next/navigation";
 import { Accordion, Card, Container } from "react-bootstrap";
+import styles from "./page.module.scss";
+import abgeschlossen from "../../../public/images/icon-abgeschlossen.svg";
+import gescheitert from "../../../public/images/icon-gescheitert.svg";
+import inArbeit from "../../../public/images/icon-in_arbeit.svg";
+import unbekannt from "../../../public/images/icon-unbekannt.svg";
+import verzoegert from "../../../public/images/icon-verzoegert_fehlt.svg";
 
 export default function CityMeasures() {
   const pathname = usePathname();
@@ -15,17 +23,62 @@ export default function CityMeasures() {
   }
 
   const numberOfElectrictyMeasures = { done: 2, inProgress: 2, late: 1, failed: 1, unknown: 5 };
-
+  const numberOfEBuildingMeasures = { done: 0, inProgress: 2, late: 0, failed: 7, unknown: 5 };
+console.log(city)
   return (
-    <Container className="mt-3">
+    <Container className={styles.container}>
       <h2 className="headingWithBar">Maßnahmen in {city.name}</h2>
-      <Accordion defaultActiveKey="0">
+      <Accordion
+        defaultActiveKey="0"
+        className={styles.accordion}
+      >
         <MeasureCard
+          eventKey="0"
           title="Strom"
           numberOfMeasures={numberOfElectrictyMeasures}
-        >        <Card.Body>Hello! I am the body</Card.Body>
+        >
+          <Card.Body>Hello! I am the body</Card.Body>
+        </MeasureCard>
+        <MeasureCard
+          eventKey="1"
+          title="Wärme und Gebäude"
+          numberOfMeasures={numberOfEBuildingMeasures}
+        >
+          <Card.Body>Hello! I am the body</Card.Body>
         </MeasureCard>
       </Accordion>
+      <div className={styles.legende}>
+        <div>
+          <Image
+            src={abgeschlossen}
+            alt="Abgeschlossene Maßnahmen"
+          ></Image>abgeschlossen
+        </div>
+        <div>
+          <Image
+                  src={inArbeit}
+                  alt="Maßnahmen in Arbeit"
+          ></Image>in Arbeit
+        </div>
+        <div>
+          <Image
+                  src={verzoegert}
+                  alt="Verzögerte Maßnahmen"
+          ></Image>verzögert
+        </div>
+        <div>
+          <Image
+                  src={gescheitert}
+                  alt="Gescheiterte Maßnahmen"
+          ></Image>gescheitert
+        </div>
+        <div>
+          <Image
+                  src={unbekannt}
+                  alt="Unbekannte Maßnahmen"
+          ></Image>unbekannt
+        </div>
+      </div>
     </Container>
   );
 }
--- frontend/app/components/MeasureCard.tsx
+++ frontend/app/components/MeasureCard.tsx
@@ -2,19 +2,25 @@ import React, { useContext } from "react";
 import Image from "next/image";
 import expandArrowDown from "../../public/images/arrow-expand-down.svg";
 import expandArrowUp from "../../public/images/arrow-expand-up.svg";
-import { Accordion, AccordionContext, Card, Container, useAccordionButton } from "react-bootstrap";
+import { Accordion, AccordionContext, Card, useAccordionButton } from "react-bootstrap";
 import styles from "./styles/MeasureCard.module.scss";
+import abgeschlossen from "../../public/images/icon-abgeschlossen.svg";
+import gescheitert from "../../public/images/icon-gescheitert.svg";
+import inArbeit from "../../public/images/icon-in_arbeit.svg";
+import unbekannt from "../../public/images/icon-unbekannt.svg";
+import verzoegert from "../../public/images/icon-verzoegert_fehlt.svg";
 
 interface MeasureCardProps {
+  eventKey: string;
   title: string;
   numberOfMeasures: { done: number; inProgress: number; late: number; failed: number; unknown: number };
   children: React.ReactNode;
 }
 
-function ContextAwareToggle({ children, eventKey, callback }) {
+const CardToggle: React.FC<{ eventKey: string }> = ({ eventKey }) => {
   const { activeEventKey } = useContext(AccordionContext);
 
-  const decoratedOnClick = useAccordionButton(eventKey, () => callback && callback(eventKey));
+  const decoratedOnClick = useAccordionButton(eventKey);
 
   const isCurrentEventKey = activeEventKey === eventKey;
 
@@ -25,9 +31,9 @@ function ContextAwareToggle({ children, eventKey, callback }) {
       onClick={decoratedOnClick}
     />
   );
-}
+};
 
-const MeasureCard: React.FC<MeasureCardProps> = ({ title, numberOfMeasures, children }) => {
+const MeasureCard: React.FC<MeasureCardProps> = ({ eventKey, title, numberOfMeasures, children }) => {
   const totalNumberOfMeasures = Object.entries(numberOfMeasures).reduce(
     (sum: number, numberOfMeasure: [string, number]) => {
       if (numberOfMeasure[0] !== "unknown") {
@@ -38,26 +44,71 @@ const MeasureCard: React.FC<MeasureCardProps> = ({ title, numberOfMeasures, chil
     0,
   );
 
+  const { activeEventKey } = useContext(AccordionContext);
+  const isCurrentEventKey = activeEventKey === eventKey;
+
   return (
-    <Card>
+    <Card className={!isCurrentEventKey ? styles.closedcard : ""}>
       <Card.Header className={styles.header}>
         <div className={styles.headertitle}>
           <h3>{title}</h3>
           <div>{totalNumberOfMeasures} Maßnahmen im Monitoring</div>
         </div>
         <div className={styles.headersecondrow}>
-          <ContextAwareToggle
-            eventKey="0"
-            callback={(x) => {
-              console.log(x);
-            }}
-          >
-            Click me!
-          </ContextAwareToggle>
+          <div className={styles.iconrow}>
+            {numberOfMeasures.done > 0 && (
+              <div className={styles.firsticon}>
+                <Image
+                  src={abgeschlossen}
+                  alt="Abgeschlossene Maßnahmen"
+                ></Image>
+                <h2>{numberOfMeasures.done}</h2>
+              </div>
+            )}
+            {numberOfMeasures.inProgress > 0 && (
+              <div className={styles.secondicon}>
+                <Image
+                  src={inArbeit}
+                  alt="Maßnahmen in Arbeit"
+                ></Image>
+                <h2>{numberOfMeasures.inProgress}</h2>
+              </div>
+            )}
+            {numberOfMeasures.late > 0 && (
+              <div className={styles.thirdicon}>
+                <Image
+                  src={verzoegert}
+                  alt="Verzögerte Maßnahmen"
+                ></Image>
+                <h2>{numberOfMeasures.late}</h2>
+              </div>
+            )}
+            {numberOfMeasures.failed > 0 && (
+              <div className={styles.fourthicon}>
+                <Image
+                  src={gescheitert}
+                  alt="Gescheiterte Maßnahmen"
+                ></Image>
+                <h2>{numberOfMeasures.failed}</h2>
+              </div>
+            )}
+            {numberOfMeasures.unknown > 0 && (
+              <div className={styles.fifthicon}>
+                <Image
+                  src={unbekannt}
+                  alt="Unbekannte Maßnahmen"
+                ></Image>
+                <h2>{numberOfMeasures.unknown}</h2>
+              </div>
+            )}
+          </div>
+          <div className={styles.toggle}>
+            <CardToggle eventKey={eventKey}></CardToggle>
+          </div>
         </div>
       </Card.Header>
-      <Accordion.Collapse eventKey="0">
-      <div className={styles.content}>{children}</div>
+      <Accordion.Collapse eventKey={eventKey}>
+        <div className={styles.content}>{children}</div>
       </Accordion.Collapse>
     </Card>
   );
