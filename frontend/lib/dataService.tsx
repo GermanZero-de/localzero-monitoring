5f724b7a9d06a22cc823651f94518e6c8324fa6b
m-vuk <Serbitas30@gmail.com>
2024-07-25 23:55:09 +0200

    removefrontend/ lib from gitignore

---
+++ frontend/lib/dataService.tsx
@@ -0,0 +1,12 @@
+export async function getCities(id:string = "") {
+    const slug = id ? `/${id}` : ""
+    const cities = await (await fetch(`${process.env.REST_API}/api/cities${slug}`, {
+        // TODO: proper url
+        cache:"no-store",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json;charset=UTF-8",
+        },
+      })).json();
+      return cities
+}
