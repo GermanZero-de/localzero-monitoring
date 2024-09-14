a9501c9f6c5cf1636db40d8ff1f644b02dc232e3
m-vuk <marko.vukadinovic@geo-dome.de>
2024-09-14 19:41:15 +0200

    [#542] text adaptation

--- frontend/app/(pages)/start/page.tsx
+++ frontend/app/(pages)/start/page.tsx
@@ -10,11 +10,9 @@ export default async function Home() {
     <>
 
       <Container>
-        <h1 className="pb-3 w-75 m-auto">LocalZero Monitoring</h1>
+        <h1 className="py-4 w-75 m-auto">LocalMonitoring</h1>
         <p className="pb-5 w-75 m-auto">
-          ... ist eine Initiative von GermanZero, um mehr Transparenz zum Fortschritt der Klimaneutralität deutscher
-          Kommunen zu schaffen. Der Fortschritt wird von ehrenamtlichen Lokalteams in den jeweiligen Kommunen regelmäßig
-          aktualisiert.
+        ... ist eine Initiative von <a href="https://localzero.net/" target="new">LocalZero</a>, um mehr Transparenz zum Fortschritt der Klimaneutralität deutscher Kommunen zu schaffen. Der Fortschritt wird von ehrenamtlichen Lokalteams in den jeweiligen Kommunen regelmäßig aktualisiert.
         </p>
         <h2 className="headingWithBar">Kommunen im Monitoring</h2>
 
